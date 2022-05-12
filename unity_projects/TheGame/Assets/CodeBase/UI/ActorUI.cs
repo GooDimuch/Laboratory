7e11a5ce1e6d6d1583621c0d2b144900dbfb1596
Jarvis <antibog8@gmail.com>
2022-05-12 18:29:41 +0300

    fixed enemy hp bar

--- unity_projects/TheGame/Assets/CodeBase/UI/ActorUI.cs
+++ unity_projects/TheGame/Assets/CodeBase/UI/ActorUI.cs
@@ -7,19 +7,15 @@ namespace CodeBase.UI {
 
 		private IHealth _health;
 
-		private void Start() {
-			if (_health == null)
-				Construct(GetComponent<IHealth>());
+		public void Construct(IHealth health) {
+			_health = health;
+			_health.HealthChanged += UpdateHpBar;
+			UpdateHpBar();
 		}
 
 		private void OnDestroy() {
 			if (_health != null)
 				_health.HealthChanged -= UpdateHpBar;
-		}
-
-		public void Construct(IHealth health) {
-			_health = health;
-			_health.HealthChanged += UpdateHpBar;
 		}
 
 		private void UpdateHpBar() => HpBar.SetValue(_health.Current, _health.Max);
